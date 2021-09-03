f25b8cf82827e70dec1bebcd170aeec6ac6302bf
Froilan Ramos <froilanmartinr@gmail.com>
2021-09-02 23:19:06 -0300

    Acomodando el codigo

--- js/index.js
+++ js/index.js
@@ -406,7 +406,7 @@ $("#modalUnoCotizacion").click(function () {
             $("#cotizacionDolar").html(`<strong>${misDatos.casa.nombre}: </strong>${misDatos.casa.venta} ARS`)
         }
     });
-    const URLGETFECHA = 'https://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires'
+    const URLGETFECHA = 'https://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires/data/180/'
     $.get(URLGETFECHA, function (respuesta2, estado) {
         if (estado === "success") {
             var cadenaFecha = respuesta2.datetime.split("T");
